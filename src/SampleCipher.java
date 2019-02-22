4fe7ab96115d26953c0c03b70d32232ab9914552
NEU_Zhangy <alice7neusoft1207@gmail.com>
2019-02-22 12:07:10 -0500

    add function to change the variable of check

--- src/SampleCipher.java
+++ src/SampleCipher.java
@@ -5,9 +5,14 @@ import javax.crypto.NoSuchPaddingException;
 
 public class SampleCipher {
 
+    private String changeString(String check) {
+        check = "AES";
+        return check;
+    }
+
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
         String check = "MD5";
-        Cipher a = Cipher.getInstance(check);
+        Cipher a = Cipher.getInstance(new SampleCipher().changeString(check));
         //int a = 0;
     }
 
